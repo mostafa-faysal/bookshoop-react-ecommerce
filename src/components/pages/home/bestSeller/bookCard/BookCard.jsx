627fcd2e80d5dff78f95a24ec83c5703d0d05c80
Mostafa Faysal <mostafafaysal419@gmail.com>
2026-06-26 02:50:59 +0300

    Add link to post details in BookCard component

--- src/components/pages/home/bestSeller/bookCard/BookCard.jsx
+++ src/components/pages/home/bestSeller/bookCard/BookCard.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function BookCard({ book }) {
   return (
-    <div className="overflow-hidden mx-5 w-48 rounded-2xl shadow-xl border-[D9176C] my-20">
-      <img
-        src={book.image}
-        alt={book.title}
-        className="object-cover w-50 h-70"
-      />
+    <div className="overflow-hidden mx-5 w-48 rounded-2xl shadow-xl border-[D9176C] my-20 h-[40vh]">
+      <Link to={`/post/${book.id}`} className="block w-full h-full">
+        <img
+          src={book.image}
+          alt={book.title}
+          className="block object-cover w-full h-full"
+        />
+      </Link>
     </div>
   );
 }
